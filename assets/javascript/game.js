01892d5da686a396b7cd88e3ff2a0179e2a60405
Angelo Bovino <angelo.bovino@outlook.com>
2016-05-28 19:04:23 -0400

    Final Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,44 +15,4 @@ $(document).ready(function() {
 		$('#userScore').html(userGems);
 }
 
-	reset();
-
-		$("#gem1").click(function() {
-	    userGems= userGems+gemNums[0];
-	    $('#userScore').html(userGems);
-	    scoreBoard(userGems);
-		})
-
-		$("#gem2").click(function() {
-	    userGems= userGems+gemNums[1];
-	    $('#userScore').html(userGems);
-	    scoreBoard(userGems);
-		})
-
-		$("#gem3").click(function() {
-	    userGems= userGems+gemNums[2];
-	    $('#userScore').html(userGems);
-	    scoreBoard(userGems);
-		})
-
-		$("#gem4").click(function() {
-	    userGems= userGems+gemNums[3];
-	    $('#userScore').html(userGems);
-	    scoreBoard(userGems);
-	})
-
-	function scoreBoard(x){
-		if (x === number) {
-			wins++;
-			$('#wins').html(wins);
-			alert('You Win!');
-			reset();
-		} else if (x > number) {
-			losses++;
-			$('#losses').html(losses);
-			alert('You Lose!')
-			reset();
-		}
-	}
-
-	});
+});
